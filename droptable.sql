dbce8c8088433263933a714f2dfda9c059aa83d5
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2024-01-22 13:59:18 +0700

    migration-v1

--- droptable.sql
+++ droptable.sql
@@ -1,3 +1,9 @@
 drop table app_port;
 drop table company_profile;
 drop table user;
+drop database dcommerce_pro_auto_manual;
+
+rm /var/log/supervisor/api_8001.log
+rm /root/config/supervisor/*_auto_*
+rm -rf /root/app/dc_manual
+rm -rf /root/api/dc_manual
