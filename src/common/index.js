fddb6d3d77144535380e79881fd32fd82cd03a71
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2025-03-06 22:34:02 +0700

    update sync user profile info with company info1

--- src/common/index.js
+++ src/common/index.js
@@ -56,11 +56,16 @@ const createApiDirectory = async (user, dbApiPort, companyProfile) => {
     // Wait for 10 seconds before proceeding
     await new Promise(resolve => setTimeout(resolve, 10000));
     // TODO: Insert company data here from object (companyProfile)
-    const sqlStatementClearDraftCompany =
-        `TRUNCATE TABLE company;`
-    const sqlStatement =
-        `INSERT INTO company (mnemonic, name, tel, email, address, province, district, village, remark, isActive, createdAt, updateTimestamp) 
-    VALUES ('BANK', '${companyProfile.companyName}', '${companyProfile.companyTelephone}', '${companyProfile.companyEmail}', '123 Main St', 'SomeProvince', 'SomeDistrict', 'SomeVillage', 'This is a remark', 1, NOW(), NOW());`;
+    const sqlStatementClearDraftCompany = `
+    SET FOREIGN_KEY_CHECKS=0; 
+    TRUNCATE TABLE company; 
+    SET FOREIGN_KEY_CHECKS=1;
+`;
+
+    const sqlStatement = `
+    INSERT INTO company (mnemonic, name, tel, email, address, province, district, village, remark, isActive, createdAt, updateTimestamp) 
+    VALUES ('BANK', '${companyProfile.companyName}', '${companyProfile.companyTelephone}', '${companyProfile.companyEmail}', '123 Main St', 'SomeProvince', 'SomeDistrict', 'SomeVillage', 'This is a remark', 1, NOW(), NOW());
+`;
 
     await linuxExecSample(`mysql -u root -D ${generatedDb} -e "${sqlStatementClearDraftCompany}"`, `Clear draft company data`);
     await linuxExecSample(`mysql -u root -D ${generatedDb} -e "${sqlStatement}"`, `Company profile sync`);
